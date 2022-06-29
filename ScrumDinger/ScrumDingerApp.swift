3b7b5c06cda7723984b49ab55e167b82eaf5950e
kensuke242424 <kennsuke242424@gmail.com>
2022-06-29 13:11:02 +0900

    Scrum Dinger 追加

---
+++ ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by 中川賢亮 on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
